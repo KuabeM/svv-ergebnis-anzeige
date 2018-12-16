1e8a85e546ad2620ebb8b522596a399d0bc37a69
Korbinian Maier <korbinian.maier@tum.de>
2018-12-16 18:13:08 +0100

    feat/backend: switched from iron to actix-web

--- src/web_backend/db.rs
+++ src/web_backend/db.rs
@@ -16,54 +16,71 @@ pub struct DbRow {
 }
 
 impl MysqlDB {
-
     pub fn create_db(name: String) -> Result<Self> {
-
         let pool = Pool::new("mysql://svv_be:2018-RUSTY-VUE!@localhost/svv_backend")?;
         let stat = format!(r"DROP TABLE IF EXISTS {}", name);
-        pool.prep_exec(stat, ())?;
+        pool.prep_exec(stat, ()).map_err(|e| {
+            BackendError::DatabaseError.context(format!("Failed to create db: {:?}", e))
+        })?;
         let stat = format!(r"CREATE TABLE {} (no int, team text, score int)", name);
-        pool.prep_exec(stat, ())?;
+        pool.prep_exec(stat, ()).map_err(|e| {
+            BackendError::DatabaseError.context(format!("Failed to init db: {:?}", e))
+        })?;
 
         Ok(MysqlDB { pool, name })
     }
 
     pub fn db_instance(name: String) -> Result<Self> {
-        let pool = Pool::new("mysql://svv_be:2018-RUSTY-VUE!@localhost/svv_backend")?;
+        let pool =
+            Pool::new("mysql://svv_be:2018-RUSTY-VUE!@localhost/svv_backend").map_err(|e| {
+                BackendError::DatabaseError.context(format!("Failed to get db instance: {:?}", e))
+            })?;
         Ok(MysqlDB { pool, name })
     }
 
     pub fn insert(&self, key: u8, name: String, score: u8) -> Result<()> {
-
-        let stat = format!(r"INSERT INTO {} (no, team, score) VALUES ('{}', '{}', {})", self.name, key, name, score);
-        self.pool.prep_exec(stat, ())?;
+        let stat = format!(
+            r"INSERT INTO {} (no, team, score) VALUES ('{}', '{}', {})",
+            self.name, key, name, score
+        );
+        self.pool.prep_exec(stat, ()).map_err(|e| {
+            BackendError::DatabaseError.context(format!("Failed to insert into db: {:?}", e))
+        })?;
 
         Ok(())
     }
 
-    pub fn update<T>(&self, key: u8, col: String, val: T) -> Result<()> 
-    where T: Display
+    pub fn update<T>(&self, key: u8, col: String, val: T) -> Result<()>
+    where
+        T: Display,
     {
-
-        let stat = format!(r"UPDATE {} SET {} = '{}' WHERE no = {}", self.name, col, val, key);
-        self.pool.prep_exec(stat, ())?;
+        let stat = format!(
+            r"UPDATE {} SET {} = '{}' WHERE no = {}",
+            self.name, col, val, key
+        );
+        self.pool.prep_exec(stat, ()).map_err(|e| {
+            BackendError::DatabaseError.context(format!("Failed to update db: {:?}", e))
+        })?;
 
         Ok(())
     }
 
     pub fn select(&self, key: u8) -> Result<Vec<DbRow>> {
+        let stat = format!(
+            r"SELECT no, team, score FROM {} WHERE no = '{}'",
+            self.name, key
+        );
 
-        let stat = format!(r"SELECT no, team, score FROM {} WHERE no = '{}'", self.name, key);
-        
         let cont: Vec<DbRow> = self.pool.prep_exec(stat, ()).map(|answer| {
-            answer.map(|x| x.unwrap()).map(|row| {
-                let (no, team, score) = mysql::from_row( row );
-                DbRow { no, team, score }
-            }).collect()
+            answer
+                .map(|x| x.unwrap())
+                .map(|row| {
+                    let (no, team, score) = mysql::from_row(row);
+                    DbRow { no, team, score }
+                })
+                .collect()
         })?;
 
-
         Ok(cont)
-
     }
 }
--- src/web_backend/teams.rs
+++ src/web_backend/teams.rs
@@ -1,57 +1,26 @@
-use iron::prelude::*;
-use iron::{status};
+use actix_web::{AsyncResponder, Error, HttpMessage, HttpRequest, HttpResponse};
+use futures::Future;
 
-use iron::method::Method;
-use iron::error::HttpError;
-
-use std::io::Read;
 use super::db::*;
 
-#[derive(Debug, Serialize, Deserialize,)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Teams {
     name_home: String,
     name_away: String,
 }
 
-
 impl Teams {
-    pub fn new(name_home: String, name_away: String) -> Self {
-        Teams {name_home, name_away}
-    }
-
-    // fn answer(&mut self, req: &mut Request) -> IronResult<Response> {
-    //     let mut buf = String::new();
-
-    //     println!("request {:?}", &req);
-    //     req.body.read_to_string(&mut buf)
-    //         .map_err(|e| IronError::new(e, "m"))?;
-    //     Ok(Response::with((status::Ok,buf)))
-    // }
-
-    pub fn post_response(&mut self, request: &mut Request) -> IronResult<Response> {
-
-        match request.method {
-            Method::Post => {
-                let mut buf = String::new();
-                request.body.read_to_string(&mut buf).map_err(|e| IronError::new(e, "Failed to read to string"))?;
-
-                *self = serde_json::from_str(&buf)
-                    .map_err(|_| IronError::new(HttpError::Io(std::io::Error::from_raw_os_error(22)), "failed to serialize"))?;
-
+    pub fn teams(req: &HttpRequest) -> Box<Future<Item = HttpResponse, Error = Error>> {
+        req.json()
+            .from_err() // convert all errors into `Error`
+            .and_then(|val: Teams| {
+                println!("model: {:?}", val);
                 let db = MysqlDB::db_instance("scoring".to_string()).unwrap();
-                db.update(1, "team".to_string(), self.name_home.clone())
-                    .map_err(|_| IronError::new(HttpError::Io(std::io::Error::from_raw_os_error(22)), "faile to insert to database"))?;
-                db.update(2, "team".to_string(), self.name_away.clone())
-                    .map_err(|_| IronError::new(HttpError::Io(std::io::Error::from_raw_os_error(22)), "faile to insert to database"))?;                    
+                db.update(1, "team".to_string(), val.name_home.clone())?;
+                db.update(2, "team".to_string(), val.name_away.clone())?;
 
-                let res = Response::with((status::Ok,buf));
-                println!("request: {:?}", &request);
-                println!("response: {:?}", &res);
-                return Ok(res)
-            }
-            _ => {
-                return Err(IronError::new(HttpError::Method, "mee"))
-            }
-        };
+                Ok(HttpResponse::Ok().json(val)) // <- send response
+            })
+            .responder()
     }
 }
