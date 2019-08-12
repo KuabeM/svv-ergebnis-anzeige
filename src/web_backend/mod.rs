c084f9c872fe57e6f3ef8e346305919ddada7500
Korbinian Maier <korbinian.maier@tum.de>
2019-08-12 21:33:57 +0000

    web_backend: use ip addr from env var

--- src/web_backend/mod.rs
+++ src/web_backend/mod.rs
@@ -1,4 +1,5 @@
 use super::errors::*;
+use std::env::var;
 
 use actix_web::{http, http::header, middleware, middleware::cors::Cors, server, App};
 
@@ -18,6 +19,8 @@ pub fn start() -> Result<()> {
     db.insert(0, "time".to_string(), 0)?;
     db.insert(1, "SVV".to_string(), 0)?;
     db.insert(2, "Gast".to_string(), 0)?;
+    
+    let ip_var = var("IP_ADDR").unwrap_or("localhost:3000".to_string());
 
     server::new(|| {
         App::new()
@@ -25,6 +28,7 @@ pub fn start() -> Result<()> {
             .configure(|app| {
                 Cors::for_app(app)
                     .allowed_origin("http://localhost:8080")
+                    .allowed_origin("http://192.168.0.104:8080")
                     .allowed_methods(vec!["POST"])
                     .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
                     .allowed_header(header::CONTENT_TYPE)
@@ -37,11 +41,11 @@ pub fn start() -> Result<()> {
                     .register()
             })
     })
-    .bind("192.168.0.41:3000")
+    .bind(&ip_var)
     .unwrap()
     .start();
 
-    println!("Started http server on 192.168.0.41:3000");
+    println!("Started http server on {}", ip_var);
     let _ = sys.run();
 
     Ok(())
